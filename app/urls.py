606951cfcaae343747db77964227fce4641adca1
LucasAndre0719 <glucasbr370@gmail.com>
2026-08-29 14:00:19 -0300

    feat: adiciona consumo de API externa

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
-
 from .viewsets import GeneroViewset,FilmeViewset,AvaliacaoViewset
+from django.urls import path
+from .views import filmes_externos
+
 
 
 router = DefaultRouter()
@@ -10,3 +12,7 @@ router.register('filmes', FilmeViewset)
 router.register('avaliacoes', AvaliacaoViewset)
 
 urlpatterns = router.urls
+
+urlpatterns+=[
+    path ('filmes_externos/', filmes_externos),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+import requests
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def filmes_externos(request):
+
+    url = 'https://www.swapi.tech/api/films/'
+
+    try:
+        resposta = requests.get(url, timeout=5)
+
+        if resposta.status_code != 200:
+            return Response(
+                {
+                'erro': 'não foi possivel consultar a API'
+                },
+                status= status.HTTP_502_BAD_GATEWAY
+            )
+
+        dados = resposta.json()
+
+        return Response(dados,status=status.HTTP_200_OK)
+    except request.RequestException:
+        return Response(
+            {
+                'erro': 'Erro ao conectar com a API externa' 
+            }, 
+            status= status.HTTP_503_SERVICE_UNAVAILABLE
+        )
+
+
+    
 
 # Create your views here.
