13330dc2c3b26031b031e0cdb146e7acb438913e
LucasAndre0719 <glucasbr370@gmail.com>
2026-08-29 14:44:41 -0300

    feat: adiciona alguns tratamentos de erros no views

--- app/views.py
+++ app/views.py
@@ -4,12 +4,24 @@ import requests
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name='titulo',
+            description='Filtra os filmes pelo título',
+            required=False,
+            type=str
+        )
+    ]
+)
 @api_view(['GET'])
 def filmes_externos(request):
 
     url = 'https://www.swapi.tech/api/films/'
+
+    titulo = request.query_params.get('titulo')
 
     try:
         resposta = requests.get(url, timeout=5)
@@ -17,23 +29,52 @@ def filmes_externos(request):
         if resposta.status_code != 200:
             return Response(
                 {
-                'erro': 'não foi possivel consultar a API'
+                    'erro': 'Não foi possível consultar a API'
                 },
-                status= status.HTTP_502_BAD_GATEWAY
+                status=status.HTTP_502_BAD_GATEWAY
             )
 
-        dados = resposta.json()
+        try:
+            dados = resposta.json()
 
-        return Response(dados,status=status.HTTP_200_OK)
-    except request.RequestException:
+        except ValueError:
+            return Response(
+                {
+                    'erro': 'A API externa retornou uma resposta inválida.'
+                },
+                status=status.HTTP_502_BAD_GATEWAY
+            )
+
+        filmes = dados.get('result', [])
+
+        if titulo:
+            filmes_filtrados = []
+
+            for filme in filmes:
+                nome_filme = filme.get('properties', {}).get('title', '')
+
+                if titulo.lower() in nome_filme.lower():
+                    filmes_filtrados.append(filme)
+
+            filmes = filmes_filtrados
+
+        if titulo and not filmes:
+            return Response(
+                {
+                    'erro': f'Nenhum filme encontrado para "{titulo}".'
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
         return Response(
-            {
-                'erro': 'Erro ao conectar com a API externa' 
-            }, 
-            status= status.HTTP_503_SERVICE_UNAVAILABLE
+            filmes,
+            status=status.HTTP_200_OK
         )
 
-
-    
-
-# Create your views here.
+    except requests.RequestException:
+        return Response(
+            {
+                'erro': 'Erro ao conectar com a API externa'
+            },
+            status=status.HTTP_503_SERVICE_UNAVAILABLE
+        )
